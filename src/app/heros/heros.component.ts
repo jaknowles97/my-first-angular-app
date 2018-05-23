456ff4e34474d82d557ea4661e6f7eb3279c34c8
Andrew <jaknowles1997@gmail.com>
2018-05-23 07:13:28 -0700

    hero component added and working

---
+++ src/app/heros/heros.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heros',
+  templateUrl: './heros.component.html',
+  styleUrls: ['./heros.component.css']
+})
+export class HerosComponent implements OnInit {
+  hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
