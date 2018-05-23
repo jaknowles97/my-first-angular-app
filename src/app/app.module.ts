32016b56043c01f9ddee4067e20f367bedeb942e
Andrew <jaknowles1997@gmail.com>
2018-05-23 08:06:17 -0700

    fixed typo causing errors with the 'heroes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HerosComponent } from './heros/heros.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HerosComponent
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
